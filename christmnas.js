f235192e24e10b3dc6cd4db73223fa480a8dafba
Daniel St. Martin <dan-iel@Daniels-MacBook-Air.local>
2019-05-17 10:13:37 -0700

    mergered this

--- christmnas.js
+++ christmnas.js
@@ -1,14 +1,14 @@
-const christmas =
-      { 
-        day1: "A parrot in an apple tree", 
-        day2: "Two tortoise eagles",
-        day3: "5 Silver loops",
-        day4: "",
-        day5: "",
-        day6: "Marco",
-        day7: "",
-        day8: "",
-        day9: "",
-        day10: "",
-        day11: "",
-        day12: ""};
+const christmas = {
+  day1: "A parrot in an apple tree",
+  day2: "Two tortoise eagles",
+  day3: "5 Silver loops",
+  day4: "",
+  day5: "",
+  day6: "lets rock this group-project",
+  day7: "",
+  day8: "",
+  day9: "",
+  day10: "jazz hands party",
+  day11: "",
+  day12: ""
+};
